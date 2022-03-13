6e9a2f66e3f0edb86220e58a82e4aeea17306034
krishna.ramachandran1973 <krishna.ramachandran1973@gmail.com>
2022-03-13 13:07:12 +0530

    first commit

---
+++ src/main/java/com/cts/gateway/OstockGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cts.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstockGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OstockGatewayApplication.class, args);
+	}
+
+}
