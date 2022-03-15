3f7c98f1f6eb780b0e5513a247eb5714bae4177a
krishna.ramachandran1973 <krishna.ramachandran1973@gmail.com>
2022-03-15 18:34:59 +0530

    Added routing through properties and coding

--- src/main/java/com/cts/gateway/OstockGatewayApplication.java
+++ src/main/java/com/cts/gateway/OstockGatewayApplication.java
@@ -2,12 +2,28 @@ package com.cts.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OstockGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OstockGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OstockGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                      .route(ps -> ps.path("/license/**")
+                                     .filters(f -> f.rewritePath("/license/(?<path>.*)", "/${path}"))
+                                     .uri("lb://licensing-service"))
+                      .route(ps -> ps.path("/organization/**")
+                                     .filters(f -> f.rewritePath("/organization/(?<path>.*)", "/${path}"))
+                                     .uri("lb://organization-service"))
+                      .build();
+
+    }
 
 }
