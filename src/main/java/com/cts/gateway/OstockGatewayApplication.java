35a4531d6eef6212678d9c4dcea5b2245458bc70
krishna.ramachandran1973 <krishna.ramachandran1973@gmail.com>
2022-03-18 10:23:28 +0530

    Added custom filters

--- src/main/java/com/cts/gateway/OstockGatewayApplication.java
+++ src/main/java/com/cts/gateway/OstockGatewayApplication.java
@@ -2,12 +2,24 @@ package com.cts.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OstockGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OstockGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OstockGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                      .route(ps -> ps.path("/license/**")
+                                     .filters(fs -> fs.rewritePath("/license/(?<path>.*)", "/${path}"))
+                                     .uri("lb://licensing-service"))
+                      .build();
+    }
 
 }
